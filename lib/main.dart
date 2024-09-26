f100ba975046f3f645cf4ebb7058551b062b7a1f
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2024-09-26 22:51:25 +0600

    Complete code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
     home: MagicBall(),
+    debugShowCheckedModeBanner: false,
   ),
 );
 
@@ -11,12 +14,58 @@ class MagicBall extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return
-    Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.cyanAccent,
+    SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Ask me anything",
+            style: TextStyle(
+              color:  Colors.white70,
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: const BallPage(),
+        backgroundColor: Colors.lightBlueAccent,
       ),
     )
       ;
   }
 
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ball = 1;
+  @override
+  Widget build(BuildContext context) {
+
+    return
+      Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: IconButton(
+                  padding: const EdgeInsets.all(30.0),
+                  onPressed: (){
+                    setState(() {
+                      ball = Random.secure().nextInt(5) + 1;
+                    });
+                  },
+                  icon: Image.asset("images/ball$ball.png"),
+                ),
+              ),
+           ]
+          ),
+      )
+    ;
+  }
 }
